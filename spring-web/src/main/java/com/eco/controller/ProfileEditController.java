ef359a08fa9327db543ae83ebfe4fee09791d578
tmxose <tmxose@naver.com>
2025-07-04 15:51:04 +0900

    shj profile controller text edit

--- spring-web/src/main/java/com/eco/controller/ProfileEditController.java
+++ spring-web/src/main/java/com/eco/controller/ProfileEditController.java
@@ -3,6 +3,7 @@ package com.eco.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,19 +18,19 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 @RequestMapping("/profileEdit")
 public class ProfileEditController {
-	
+
 	@GetMapping("")
-	public String profilePage(HttpSession session) {
+	public String profilePage(HttpSession session, Model model) {
 		String type = (String) session.getAttribute("userType");
 		ProfileEditDTO dto = new ProfileEditDTO();
-		if(type =="common") {
+		if ("common".equals(type)) {
 			UserVO currentUser = (UserVO) session.getAttribute("currentUserInfo");
 			dto.setId(currentUser.getUser_id());
 			dto.setPw(currentUser.getUser_pw());
 			dto.setNm(currentUser.getUser_nm());
 			dto.setAddr(currentUser.getUser_addr());
 			dto.setMail(currentUser.getUser_mail());
-		}else if(type =="staff" || type =="admin") {
+		} else if ("staff".equals(type) || "admin".equals(type)) {
 			StaffVO currentUser = (StaffVO) session.getAttribute("currentUserInfo");
 			dto.setId(currentUser.getStaff_id());
 			dto.setPw(currentUser.getStaff_pw());
@@ -37,22 +38,39 @@ public class ProfileEditController {
 			dto.setAddr(currentUser.getStaff_addr());
 			dto.setMail(currentUser.getStaff_role());
 		}
-		session.setAttribute("profileInfo", dto);
-		
+
+		model.addAttribute("profileInfo", dto);
+
 		return "user/profileEdit";
 	}
-	
+
 	@PostMapping("")
 	public String profileEdit(ProfileEditDTO dto, HttpSession session) {
 		String type = (String) session.getAttribute("userType");
-		if(type =="user") {
+		if ("common".equals(type)) {
 			UserVO vo = new UserVO();
+			vo.setUser_id(dto.getId());
+			vo.setUser_pw(dto.getPw());
+			vo.setUser_nm(dto.getNm());
+			vo.setUser_addr(dto.getAddr());
+			vo.setUser_mail(dto.getMail());
+			// TODO: userService.update(vo);
 			
+			// 수정된 정보 다시 세션에 저장
+			session.setAttribute("currentUserInfo", vo);
 
-		}else if(type =="staff" || type =="admin") {
+		} else if ("staff".equals(type) || "admin".equals(type)) {
 			StaffVO vo = new StaffVO();
+			vo.setStaff_id(dto.getId());
+			vo.setStaff_pw(dto.getPw());
+			vo.setStaff_nm(dto.getNm());
+			vo.setStaff_addr(dto.getAddr());
+			vo.setStaff_role(dto.getMail());
+			// TODO: staffService.update(vo);
+			// 세션 갱신!
+	        session.setAttribute("currentUserInfo", vo);
 		}
-		
-		return "user/profileEdit";
+
+		return "redirect:/profileEdit";
 	}
 }

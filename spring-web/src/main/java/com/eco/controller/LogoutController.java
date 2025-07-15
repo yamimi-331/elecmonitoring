8d005cb4c891ab1958548d57c532793e009ced35
tmxose <tmxose@naver.com>
2025-07-15 11:02:43 +0900

    shj 로그아웃 시에 빈화면 공백 생기는 부분 수정

--- spring-web/src/main/java/com/eco/controller/LogoutController.java
+++ spring-web/src/main/java/com/eco/controller/LogoutController.java
@@ -3,9 +3,9 @@ package com.eco.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.log4j.Log4j;
 
@@ -16,20 +16,20 @@ public class LogoutController {
 
 	//로그아웃
     @GetMapping("")
-    public String logout(HttpSession session, RedirectAttributes redirectAttrs) {
+    public String logout(HttpSession session, Model model) {
     	log.info("로그아웃 시도");
         session.removeAttribute("currentUserInfo");
         session.removeAttribute("userType");
-        redirectAttrs.addFlashAttribute("message", "로그아웃이 완료되었습니다.");
-        return "redirect:/"; 
+        model.addAttribute("message", "로그아웃이 완료되었습니다.");
+        return "index"; 
     }
     
     @GetMapping("/auto")
-    public String logoutAuto(HttpSession session,  RedirectAttributes redirectAttrs) {
+    public String logoutAuto(HttpSession session,  Model model) {
     	log.info("로그인 세션 만료");
         session.removeAttribute("currentUserInfo");
         session.removeAttribute("userType");
-        redirectAttrs.addFlashAttribute("message", "로그인 유지시간이 만료되어 자동 로그아웃 되었습니다.");
-        return "redirect:/"; 
+        model.addAttribute("message", "로그인 유지시간이 만료되어 자동 로그아웃 되었습니다.");
+        return "index"; 
     }
 }
